0bfd4ecc401f19b045e3228881aae6c5eb1af66e
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-07-02 18:14:33 -0500

    commit on master

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const app = express();
 app.use(bodyParser.json(),cors());
 
 app.post('/test', (req, res) => {
-  console.log(req.body);
-  res.status(200).send('200 Data Received');
+  console.log("Type Recieved "+ req.body.type);
+  console.log("Code Recieved "+ req.body.code);
+  res.status(200).send('200 Data Received' + req.body);
 });
 
 const PORT = process.env.PORT || 4000;
