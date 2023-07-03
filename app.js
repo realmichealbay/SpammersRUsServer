0fb6986dd244a3197bf69d0a6a95d23818d6954e
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-07-02 20:03:34 -0500

    commit on master

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const bodyParser = require('body-parser');
 const https = require('https');
 const fs = require('fs');
 const cors = require("cors");
+const { check, validationResult } = require('express-validator');
 
 //testing github actions/
 
 const app = express();
 
-app.use(bodyParser.json(),cors());
+app.use(bodyParser.json(),cors({origin: 'https://spammersrus.com'}));
 
-app.post('/test', (req, res) => {
-  console.log("Type Recieved "+ req.body.type);
-  console.log("Code Recieved "+ req.body.code);
-  res.status(200).send('200 Data Received' + req.body);
+
+app.post('/test', [
+    check('type').isString(),
+    check('code').isString()
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    console.log("Type Recieved "+ req.body.type);
+    console.log("Code Recieved "+ req.body.code);
+    res.status(200).send('200 Data Received' + req.body);
 });
 
 const PORT = process.env.PORT || 4000;
